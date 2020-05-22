b20a716b81f08459568a864067e8d9fe1b887e78
tianli <a464015561@qq.com>
2020-05-22 17:07:39 +0800

    no message

--- IOS-PROJECT-TEMPLAT功能版/IOS-PROJECT-TEMPLATE/Class/Home/V/MineTableViewCell.m
+++ IOS-PROJECT-TEMPLAT功能版/IOS-PROJECT-TEMPLATE/Class/Home/V/MineTableViewCell.m
@@ -7,14 +7,28 @@
 //
 
 #import "MineTableViewCell.h"
-
+#import "MineDataModel.h"
 @implementation MineTableViewCell
 
 - (void)awakeFromNib {
     [super awakeFromNib];
+    [self requsetData];
     // Initialization code
 }
-
+-(void)requsetData
+{
+    UserModel * model =[[UserManager shareInstance]getUser];
+       NSDictionary* param_dic =@{@"userPhone":model.userPhone};
+       [RequestHelp POST:SELECT_USERINFO_url parameters:param_dic success:^(id result) {
+           MKLog(@"%@",result);
+         MineDataModel *model =[MineDataModel yy_modelWithJSON:result];
+            [self.HeadImg sd_setImageWithURL:[NSURL URLWithString:model.headAddress] placeholderImage:[UIImage imageNamed:@"临时占位图"]];
+            self.PhoneLab.text=model.userPhone;
+           self.NameLab.text=model.realName;
+       } failure:^(NSError *error) {
+           
+       }];
+}
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
 
